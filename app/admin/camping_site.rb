aaddd6ff52c38846d96dcba4bf8ff7d143113824
Raghu Betina <raghu@firstdraft.com>
2016-12-03 20:57:59 +0000

    Whitelist columns in CampingSite for ActiveAdmin

--- app/admin/camping_site.rb
+++ app/admin/camping_site.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CampingSite do
 
+ permit_params :capacity, :review_id, :park_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
