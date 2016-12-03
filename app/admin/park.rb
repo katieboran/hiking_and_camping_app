77b90126572b25c237a407fb8c05c2c89d0fdb69
Raghu Betina <raghu@firstdraft.com>
2016-12-03 20:57:55 +0000

    Whitelist columns in Park for ActiveAdmin

--- app/admin/park.rb
+++ app/admin/park.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Park do
 
+ permit_params :state_id, :address, :dates_of_operation, :image, :activities
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
