7e532d8cfbc488cb2ffd6e35eda825189be3f157
Raghu Betina <raghu@firstdraft.com>
2016-12-03 20:57:48 +0000

    Whitelist columns in HikingTrail for ActiveAdmin

--- app/admin/hiking_trail.rb
+++ app/admin/hiking_trail.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register HikingTrail do
 
+ permit_params :address, :distance, :difficulty, :park_id, :review_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
